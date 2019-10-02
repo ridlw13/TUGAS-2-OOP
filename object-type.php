944ef6425728b74743fa8d6c35dea6588e66fb47
ridlw13 <ridlwanbaks@mail.unpas.ac.id>
2019-10-02 12:17:56 +0700

    Membuat file baru object-type.php dan belajar membuat object-type di php

---
+++ object-type.php
@@ -0,0 +1,43 @@
+<?php
+class Produk {
+
+    public $judul ,
+            $penulis ,
+            $penerbit ,
+            $harga ;
+
+    public function __construct($judul = "judul", $penerbit= "penerbit" , $penulis = "penulis", $harga = 0){
+        $this->judul = $judul;
+        $this->penulis = $penulis;
+        $this->penerbit = $penerbit;
+        $this->harga = $harga;
+        
+    }
+    public function sayHello(){
+        return "Hello World";
+    }
+
+    public function getLabel() {
+        return "$this->penulis, $this->penerbit";
+    }
+
+}
+class CetakInfoProduk{
+    public function cetak( $produk ){
+        $str = "{$produk->judul}| {$produk->getlabel()} | (Rp. {$produk->harga})";
+        return $str;
+    }
+}
+
+$produk3 = new Produk("Naruto", "Masashi Kishimoto", "Shonen Jump", 3000);
+$produk4 = new Produk("Uncharted", "Neli K", "Sony Entertaiment", 35000);
+$produk5 = new Produk("Dragon Ball");
+
+echo "Komik : " . $produk3->getLabel();
+echo "<br>";
+echo "Game : " . $produk4->getLabel();
+echo "<br>";
+
+$infoProduk1 = new CetakInfoProduk();
+echo $infoProduk1 ->cetak($produk5);
+?>
