7b2e1ba3a97ea3796bcc603bdd7a0852bcc40b24
ridlw13 <ridlwanbaks@mail.unpas.ac.id>
2019-10-03 10:24:20 +0700

    membuat file baru user.php yang didalamnya terdapat class User

---
+++ namespace/Apps/Produk/User.php
@@ -0,0 +1,8 @@
+<?php
+
+
+class User {
+    public function __construct(){
+        echo "ini adalah kelas " . __CLASS__;
+    }
+}
--- namespace/index.php
+++ namespace/index.php
@@ -2,12 +2,17 @@
 
 require_once 'Apps/init.php';
 
-$produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000,  0);
-$produk2 = new Game("Uncharted", "Neli K", "Sony Entertaiment", 35000, 50);
-// $produk3 = new Produk("Dragon Ball");
+// $produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000,  0);
+// $produk2 = new Game("Uncharted", "Neli K", "Sony Entertaiment", 35000, 50);
+// // $produk3 = new Produk("Dragon Ball");
 
-$cetakProduk = new CetakInfoProduk();
-$cetakProduk->tambahProduk ($produk1);
-$cetakProduk->tambahProduk ($produk2);
-echo $cetakProduk->cetak();
+// $cetakProduk = new CetakInfoProduk();
+// $cetakProduk->tambahProduk ($produk1);
+// $cetakProduk->tambahProduk ($produk2);
+// echo $cetakProduk->cetak();
+
+
+// echo "<hr>"
+
+new User();
 ?>
